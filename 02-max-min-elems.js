6d8673086998fe35823565534496dc707fd93d02
mariam <mariamkhachatryan031@gmail.com>
2022-07-03 00:03:28 +0400

    Fix

--- 02-max-min-elems.js
+++ 02-max-min-elems.js
@@ -1,18 +1,18 @@
 // 2. Find the smallest and the largest number from the supplied numeric array. Return an array containing these numbers։
 // Գտնել զանգվածի ամենափոքր և ամենամեծ տարրերը և վերադարձնել նոր զանգվածի մեջ։
 
-function findMaxAndMinElements(array){
+function findMaxAndMinElements(array) {
     let max = array[0];
     let min = array[0];
 
     for (let i = 0; i < array.length; i++) {
-        if(max<array[i]){
-            max=array[i];
-        } if(min>array[i]){
+        if (max < array[i]) {
+            max = array[i];
+        } if (min > array[i]) {
             min = array[i];
         }
     }
-    return [min,max];
+    return [min, max];
 }
 let numbers = [14, 28, 3, 8, 2];
 let result = findMaxAndMinElements(numbers);
--- 16-more-than-60-scored.js
+++ 16-more-than-60-scored.js
@@ -1,18 +1,91 @@
 // 16. Return array of students that have any evaluation more than 60․
 // Վերադարձնել այն ուսանողների ցուցակը, որոնք ունեն որևէ արդյունք որը մեծ է 60-ից։
-
+const evaluations = [
+    {
+        studentId: 1, // Unique identifier for each student
+        studentName: "John Doe",
+        courseId: 1, // Unique identifier for each course
+        courseName: "JS Bootcamp",
+        score: 70
+    },
+    {
+        studentId: 1, // Unique identifier for each student
+        studentName: "John Doe",
+        courseId: 2, // Unique identifier for each course
+        courseName: "JS Level 0",
+        score: 60
+    },
+    {
+        studentId: 1, // Unique identifier for each student
+        studentName: "John Doe",
+        courseId: 3, // Unique identifier for each course
+        courseName: "JS Level 1",
+        score: 50
+    },
+    {
+        studentId: 2, // Unique identifier for each student
+        studentName: "Tom Colins",
+        courseId: 1, // Unique identifier for each course
+        courseName: "JS Bootcamp",
+        score: 60
+    },
+    {
+        studentId: 2, // Unique identifier for each student
+        studentName: "Tom Colins",
+        courseId: 2, // Unique identifier for each course
+        courseName: "JS Level 0",
+        score: 50
+    },
+    {
+        studentId: 2, // Unique identifier for each student
+        studentName: "Tom Colins",
+        courseId: 3, // Unique identifier for each course
+        courseName: "JS Level 1",
+        score: 40
+    },
+    {
+        studentId: 3, // Unique identifier for each student
+        studentName: "Sarah Conor",
+        courseId: 1, // Unique identifier for each course
+        courseName: "JS Bootcamp",
+        score: 45
+    },
+    {
+        studentId: 3, // Unique identifier for each student
+        studentName: "Sarah Conor",
+        courseId: 2, // Unique identifier for each course
+        courseName: "JS Level 0",
+        score: 35
+    },
+    {
+        studentId: 3, // Unique identifier for each student
+        studentName: "Sarah Conor",
+        courseId: 3, // Unique identifier for each course
+        courseName: "JS Level 1",
+        score: 25
+    }
+];
 function moreThan60ScoredStudents(evaluations) {
     let studentList = [];
     for (let i = 0; i < evaluations.length; i++) {
-        if (evaluations[i].score > 50) {
-            studentList.push({
-                "studentId": evaluations[i].studentId,
-                "studentName": evaluations[i].studentName,
-                "score": evaluations[i].score
-            });
+        if (evaluations[i].score > 60) {
+            let alreadyInList = false;
+            for (let j = 0; j < studentList.length; j++) {
+                if (evaluations[i].studentId === studentList[j].studentId) {
+                    alreadyInList = true;
+                    break;
+                }
+            }
+            if (!alreadyInList) {
+                studentList.push({
+                    "studentId": evaluations[i].studentId,
+                    "studentName": evaluations[i].studentName
+                });
+            }
         }
     }
     return studentList;
 }
+
 let res = moreThan60ScoredStudents(evaluations);
 console.log(res);
