a0f13aad5a66c32c07ae1c2838dd2eca696175ca
mariamkhachatryan9 <98641342+mariamkhachatryan9@users.noreply.github.com>
2022-07-03 00:42:00 +0400

    Fix

--- 13-obj-total-sum.js
+++ 13-obj-total-sum.js
@@ -1,60 +1,9 @@
-const evaluations = [
-    {
-        studentId: 1, // Unique identifier for each student
-        courseName: "John Doe", // Name of the student
-        payedAmount: 1600, // Amount payed with this transaction
-        year: 2020, // The payment year
-    },
-    {
-        studentId: 1, // Unique for each student
-        courseName: "John Doe",
-        payedAmount: 1500,
-        year: 2021
-    },
-    {
-        studentId: 1, // Unique for each student
-        courseName: "John Doe",
-        payedAmount: 1400,
-        year: 2022
-    },
-    {
-        studentId: 1, // Unique for each student
-        courseName: "John Doe",
-        payedAmount: 1300,
-        year: 2023
-    },
-    {
-        studentId: 2, // Unique for each student
-        courseName: "Lu Kang",
-        payedAmount: 1500,
-        year: 2020
-    },
-    {
-        studentId: 2, // Unique for each student
-        courseName: "Lu Kang",
-        payedAmount: 1400,
-        year: 2021
-    },
-    {
-        studentId: 2, // Unique for each student
-        courseName: "Lu Kang",
-        payedAmount: 1300,
-        year: 2023
-    },
-    {
-        studentId: 2, // Unique for each student
-        courseName: "Lu Kang",
-        payedAmount: 1200,
-        year: 2023
-    }
-];
-
 // Calculate the total sum of payment amounts made by each student. We need to return an array that 
 // contains a list of information about students, including the total paid amount by the student.
 // Հաշվել յուրաքանչյուր ուսանողի կողմից վճարված ամբողջ գումարը։ Անհրաժեշտ է վերադարձնել ուսանողների ցուցակը (array),
 //  որը ընդգրկում է ուսանողների տվյալները, նեռարյալ ուսանողի կողմից ամբողջ վճարված գումարը։
 
-function getStudentsAndevaluations(evaluations) {
+function getStudentsAndEvaluations(evaluations) {
     let studentList = [];
     let sum = 0;
     for (let i = 0; i < evaluations.length; i++) {
@@ -76,5 +25,6 @@ function getStudentsAndevaluations(evaluations) {
     }
     return studentList;
 }
-let res = getStudentsAndevaluations(evaluations);
+
+let res = getStudentsAndEvaluations(evaluations);
 console.log(res);
