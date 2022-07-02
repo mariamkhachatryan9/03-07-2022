2b0b940e1d9158955864206e7a47164ba8c41f17
mariamkhachatryan9 <98641342+mariamkhachatryan9@users.noreply.github.com>
2022-07-03 00:41:00 +0400

    Fix

--- 12-obj-average.js
+++ 12-obj-average.js
@@ -1,14 +1,14 @@
 //12. Calculate the average of all evaluations
 // Հաշվել բոլոր կատարված վճարուների թվաբանական միջինը
 
-function getAverageOfevaluations(evaluations) {
+function getAverageOfPayments(payments) {
     let sum = 0;
     let count = 0;
-    for (let i = 0; i < evaluations.length; i++) {
-        sum += evaluations[i].payedAmount;
+    for (let i = 0; i < payments.length; i++) {
+        sum += payments[i].payedAmount;
         count++;
     }
     return sum / count;
 }
-let res = getAverageOfevaluations(evaluations);
+let res = getAverageOfPayments(payments);
 console.log(res);
