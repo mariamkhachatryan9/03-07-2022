0a352e3c88d3431340fc5760b60c0f9735a76b52
mariamkhachatryan9 <98641342+mariamkhachatryan9@users.noreply.github.com>
2022-07-03 00:37:51 +0400

    Fix

--- 10-obj-sum.js
+++ 10-obj-sum.js
@@ -1,12 +1,12 @@
 // 10. Calculate the sum of all evaluations 
 // Հաշվել վճարումների ամբողջ գումարը
 
-function sumOfevaluations(evaluations){
+function sumOfPayments(payments){
     let sum = 0;
-    for (let i = 0; i < evaluations.length; i++) {
-        sum += evaluations[i].payedAmount;
+    for (let i = 0; i < payments.length; i++) {
+        sum += payments[i].payedAmount;
     }
     return sum;
 }
-let res = sumOfevaluations(evaluations);
+let res = sumOfPayments(payments);
 console.log(res);
