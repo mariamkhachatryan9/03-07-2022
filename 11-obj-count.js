d9a5e2ec430ef5e953eadb8f7e99ea06ec9fc738
mariamkhachatryan9 <98641342+mariamkhachatryan9@users.noreply.github.com>
2022-07-03 00:40:07 +0400

    Fix

--- 11-obj-count.js
+++ 11-obj-count.js
@@ -1,14 +1,14 @@
 //11. Find the count of all evaluations that are more than 1500
 // Գտնել բոլոր այն վճարումների քանակը, որոնք ավելին են 1500-ից
 
-function countOfevaluations(evaluations) {
+function countOfPayments(payments) {
     let count = 0;
-    for (let i = 0; i < evaluations.length; i++) {
-        if (evaluations[i].payedAmount > 1500){
+    for (let i = 0; i < payments.length; i++) {
+        if (payments[i].payedAmount > 1500){
             count++;
         }
     }
     return count;
 }
-let res = countOfevaluations(evaluations);
+let res = countOfPayments(payments);
 console.log(res);
